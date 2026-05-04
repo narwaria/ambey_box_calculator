a981439d5346adae26d20a1988bbd6c92baf0de1
DDEV User <nobody@example.com>
2026-05-04 14:21:37 +0000

    Fixed the download button UI

--- src/Controller/QuoteConfirmationController.php
+++ src/Controller/QuoteConfirmationController.php
@@ -4,6 +4,7 @@ namespace Drupal\ambey_box_calculator\Controller;
 
 use Drupal\ambey_box_calculator\PricingCalculator;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 
 class QuoteConfirmationController extends ControllerBase {
   public function view(string $quote_id): array {
@@ -12,6 +13,16 @@ class QuoteConfirmationController extends ControllerBase {
       return ['#markup' => '<div class="text-red-600">Quote not found.</div>'];
     }
     $quote['total_with_gst'] = PricingCalculator::formatIndianNumber($quote['total_with_gst'] ?? 0);
-    return ['#theme' => 'quote_confirmation', '#quote' => $quote];
+    $download_url = Url::fromRoute('ambey_box_calculator.download', ['quote_id' => $quote['uuid']])->toString();
+    $quote['download_url'] = $download_url;
+
+    return [
+      '#theme' => 'quote_confirmation',
+      '#quote' => $quote,
+      '#download_url' => $download_url,
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
   }
 }
